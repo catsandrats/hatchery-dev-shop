1fb47bcac36d8ef520578c0d5e3a22571430c555
Dana Spiegel <dana@sociabledesign.com>
2012-09-15 03:11:55 -0400

    moved configuration file generation to templates

---
+++ etc/profile.d/environment.sh
@@ -0,0 +1,14 @@
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(parse_git_branch)$ "
+
+export HISTIGNORE="&:ls:[bf]g:exit"
+export HISTFILESIZE=10000
+export HISTCONTROL=erasedups
+export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+
+export DB_SERVER=REPLACE_DB_SERVER
+export DJANGO_SOCKET=/var/run/hatchery/$USER
+alias runserver="./manage.py runfcgi socket=$DJANGO_SOCKET umask=0002 method=prefork daemonize=false"
